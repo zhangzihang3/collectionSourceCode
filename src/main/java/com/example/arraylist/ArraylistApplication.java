96a844b8a2d9cd2452c71cfa8493b86d78f59b0e
zhangzixing <3273448110@qq.com>
2021-03-06 10:55:09 +0800

    version 2021-3-6

---
+++ src/main/java/com/example/arraylist/ArraylistApplication.java
@@ -0,0 +1,13 @@
+package com.example.arraylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArraylistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArraylistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/arraylist/ArraylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arraylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArraylistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
